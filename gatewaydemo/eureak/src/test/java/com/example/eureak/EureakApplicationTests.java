9eac5640dcdb5e0ea73b093dff4029c77988aa15
terry0527 <392835832@qq.com>
2022-04-09 20:51:45 +0800

    提交

---
+++ gatewaydemo/eureak/src/test/java/com/example/eureak/EureakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
