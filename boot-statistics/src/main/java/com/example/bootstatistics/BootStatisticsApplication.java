b13652c39caf374739ae401d78cdfb54470297d4
terry0527 <392835832@qq.com>
2022-04-11 23:52:39 +0800

    statistics

---
+++ boot-statistics/src/main/java/com/example/bootstatistics/BootStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStatisticsApplication.class, args);
+    }
+
+}
