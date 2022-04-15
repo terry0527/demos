b3677c629d5c7f120d9da38fd1ccf67c6d924e43
terry0527 <392835832@qq.com>
2022-04-15 20:43:41 +0800

    leetcode

---
+++ boot-leetcode/src/main/java/com/example/bootleetcode/BootLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLeetcodeApplication.class, args);
+    }
+
+}
---
+++ boot-leetcode/src/main/java/com/example/bootleetcode/demo/Leet70.java
@@ -0,0 +1,57 @@
+package com.example.bootleetcode.demo;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ */
+public class Leet70 {
+    public static void main(String[] args) {
+        climbStairs(3);
+    }
+
+    private static Map<Integer,Integer> storMap = new HashMap<>();
+
+    public static int climbStairs(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(null!=storMap.get(n)){
+            return storMap.get(n);
+        }else {
+            int result = climbStairs(n-1)+climbStairs(n-2);
+            storMap.put(n,result);
+            System.out.println(result);
+            return result;
+        }
+    }
+
+    //循环的解法，自底上累加
+
+    public static int climbStairs2(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        int result = 0;
+        int pre = 2;
+        int prePrev= 1;
+        for (int i = 3; i <=n; i++) {
+            result = pre +prePrev;
+            prePrev = pre;
+            pre = result;
+        }
+        System.out.println("climbStairs2"+result);
+        return result;
+    }
+}
