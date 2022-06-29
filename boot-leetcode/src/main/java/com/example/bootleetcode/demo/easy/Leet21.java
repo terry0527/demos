a230f823b645f1ea20547014854ce67b8304ff2d
terry0527 <392835832@qq.com>
2022-06-29 21:56:51 +0800

    leetcode

--- boot-leetcode/src/main/java/com/example/bootleetcode/demo/easy/Leet21.java
+++ boot-leetcode/src/main/java/com/example/bootleetcode/demo/easy/Leet21.java
@@ -9,44 +9,44 @@ package com.example.bootleetcode.demo.easy;
 public class Leet21 {
 
     //循环+双指针
-    public ListNode merge(ListNode l1,ListNode l2){
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
-        ListNode resultNode = new ListNode(0);
-        ListNode p = resultNode;
-        while(l1 != null && l2 !=null){
-            if(l1.val < l2.val){
-                p.next = l1;
-                l1 = l1.next;
-            }else{
-                p.next = l2;
-                l2 = l2.next;
-            }
-            p.next;
-        }
-        if(l1 != null){
-            p.next = l1;
-        }
-        if(l2 != null){
-            p.next = l2;
-        }
-        return resultNode.next;
-    }
-
-    //递归
-    public ListNode merge2(ListNode l1,ListNode l2){
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
-        if(l1.val < l2.val){
-            l1.next = mergeTwoLists(l1.next,l2);
-            return l1;
-        }else{
-            l2.next = mergeTwoLists(l1,l2.next);
-            return l2;
-        }
-    }
+//    public ListNode merge(ListNode l1,ListNode l2){
+//        if(l1==NULL)
+//            return l2;
+//        if(l2==NULL)
+//            return l1;
+//        ListNode resultNode = new ListNode(0);
+//        ListNode p = resultNode;
+//        while(l1 != null && l2 !=null){
+//            if(l1.val < l2.val){
+//                p.next = l1;
+//                l1 = l1.next;
+//            }else{
+//                p.next = l2;
+//                l2 = l2.next;
+//            }
+//            p.next;
+//        }
+//        if(l1 != null){
+//            p.next = l1;
+//        }
+//        if(l2 != null){
+//            p.next = l2;
+//        }
+//        return resultNode.next;
+//    }
+//
+//    //递归
+//    public ListNode merge2(ListNode l1,ListNode l2){
+//        if(l1==NULL)
+//            return l2;
+//        if(l2==NULL)
+//            return l1;
+//        if(l1.val < l2.val){
+//            l1.next = mergeTwoLists(l1.next,l2);
+//            return l1;
+//        }else{
+//            l2.next = mergeTwoLists(l1,l2.next);
+//            return l2;
+//        }
+//    }
 }
