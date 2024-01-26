1afccd13a55d00c331404a558a9dd09fccd540bd
terry0527 <392835832@qq.com>
2024-01-26 22:30:23 +0800

    111

---
+++ boot-work/src/test/java/com/example/bootwork/BootWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
