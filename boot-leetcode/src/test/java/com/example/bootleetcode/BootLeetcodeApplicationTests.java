51869fa7f11044db1418297fb2ffce36da4cb423
terry0527 <392835832@qq.com>
2024-02-20 21:28:59 +0800

    111

---
+++ boot-leetcode/src/test/java/com/example/bootleetcode/BootLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
